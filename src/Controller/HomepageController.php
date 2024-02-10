9990ad67f29746ba1b1bc7131ffa25fa15c784af
youssef <youssef.azzouz@esprit.tn>
2024-02-10 00:49:42 +0000

    login register crud almost done

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomepageController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_homepage')]
+    public function index(): Response
+    {
+        return $this->render('homepage/index.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    #[Route('/back', name: 'app_back')]
+    public function back(): Response
+    {
+        return $this->render('backbase.html.twig');
+    }
+}
